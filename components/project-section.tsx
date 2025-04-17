f2a8580d864189f153a4c46156043f7f63b0c63b
manzrowr <alokkopling123@gmail.com>
2025-04-17 20:30:31 +0700

    Update project-section.tsx

--- components/project-section.tsx
+++ components/project-section.tsx
@@ -70,7 +70,7 @@ const ProjectSection: React.FC = () => {
     >
       <div ref={sectionRef} className="container mx-auto">
         <div className="flex justify-center mb-2">
-          <SectionHeading>🚀 Projects</SectionHeading>
+          <SectionHeading>🚀 My Friends</SectionHeading>
           <BurstBload2 />
         </div>
 
